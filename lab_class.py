2c09efbe7646411d604f9187a9def175440eb273
GizatPP <120188378+GizatPP@users.noreply.github.com>
2023-02-06 23:12:25 +0600

    Update lab_class.py

--- lab_class.py
+++ lab_class.py
@@ -13,5 +13,15 @@ obj = Shape.Square(4)
 print(obj)
 obj.area(45)
 
-class
+class Employee:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def __str__(self):
+        return f"{self.name}({self.age})"
+
+obj = Employee("Gizat", 22)
+print(obj)
+
 
