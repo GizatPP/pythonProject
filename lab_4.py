a6d9d9a72711e089c73500281bbdaed593b07128
GizatPP <120188378+GizatPP@users.noreply.github.com>
2023-02-19 19:43:02 +0600

    Update lab_4.py

--- lab_4.py
+++ lab_4.py
@@ -69,3 +69,21 @@ def f4calc(n_sides, s_length):
 n_sides = float(input("Input number of sides: "))
 s_length = float(input("Input the length of a side: "))
 f4calc(n_sides, s_length)
+
+#json
+import json
+jsondata = open('sample-data.json').read()
+aasas = json.loads(jsondata)
+print("=======================================================================================" "\n"
+      "DN                                                 Description           Speed    MTU" "\n"
+      "-------------------------------------------------- --------------------  ------  ------"
+      )
+imdata = aasas["imdata"]
+for i in imdata:
+        dn = i["l1PhysIf"]["attributes"]["dn"]
+        descr = i["l1PhysIf"]["attributes"]["descr"]
+        speed = i["l1PhysIf"]["attributes"]["speed"]
+        mtu = i["l1PhysIf"]["attributes"]["mtu"]
+        print("{0} {1} {2} {3}".format(dn,descr,speed,mtu))
+        if dn=="topology/pod-1/node-201/sys/phys-[eth1/35]":
+            break
