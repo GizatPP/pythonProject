945ac22f718779f7b59cd59f6c932dcda12b2f63
GizatPP <120188378+GizatPP@users.noreply.github.com>
2023-04-04 17:33:45 +0600

    Update lab_9.py

--- lab_9.py
+++ lab_9.py
@@ -1,3 +1,5 @@
+
+'''
 #racer
 # Imports
 import pygame, sys
@@ -19,10 +21,12 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 # Other Variables for use in the program
-SCREEN_WIDTH = 400
+SCREEN_WIDTH = 775
 SCREEN_HEIGHT = 600
 SPEED = 5
+SPEED_COIN = 5
 SCORE = 0
+COUNTER = 0
 
 # Setting up Fonts
 font = pygame.font.SysFont("Verdana", 60)
@@ -32,37 +36,41 @@ game_over = font.render("Game Over", True, BLACK)
 background = pygame.image.load("AnimatedStreet.png")
 
 # Create a white screen
-DISPLAYSURF = pygame.display.set_mode((400, 600))
+DISPLAYSURF = pygame.display.set_mode((775, 600))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
+
+# NEW CLASS OF COIN
+class Coin(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("coin.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+    def move(self):
+        self.rect.move_ip(0, SPEED_COIN)
+        if (self.rect.top > 600):
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("Enemy.png")
+        self.image = pygame.image.load("enemy.png")
         self.rect = self.image.get_rect()
         self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 
     def move(self):
         global SCORE
         self.rect.move_ip(0, SPEED)
-        if (self.rect.bottom > 600):
+        if (self.rect.top > 600):
             SCORE += 1
             self.rect.top = 0
             self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 
 
-class Coin(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.x = x
-        self.y = y
-        self.weight = random.randint(1, 10)  # generate a random weight for the coin between 1 and 10
-        self.image = pygame.image.load("coin.png")
-        
-    def draw(self, screen):
-        screen.blit(self.image, (self.x, self.y))
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -72,6 +80,10 @@ class Player(pygame.sprite.Sprite):
 
     def move(self):
         pressed_keys = pygame.key.get_pressed()
+        # if pressed_keys[K_UP]:
+        # self.rect.move_ip(0, -5)
+        # if pressed_keys[K_DOWN]:
+        # self.rect.move_ip(0,5)
 
         if self.rect.left > 0:
             if pressed_keys[K_LEFT]:
@@ -80,26 +92,23 @@ class Player(pygame.sprite.Sprite):
             if pressed_keys[K_RIGHT]:
                 self.rect.move_ip(5, 0)
 
-coins = [] # create an empty list to hold the coins
 
-# generate 10 coins with random positions and add them to the list
-for i in range(10):
-    x = random.randint(0, SCREEN_WIDTH)
-    y = random.randint(0, SCREEN_HEIGHT)
-    coin = Coin(x, y)
-    coins.append(coin)
 # Setting up Sprites
 P1 = Player()
 E1 = Enemy()
 C1 = Coin()
+
+
 # Creating Sprites Groups
 enemies = pygame.sprite.Group()
 enemies.add(E1)
-enemies.add(C1)
+
+coins = pygame.sprite.Group()
+coins.add(C1)
+
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
 all_sprites.add(E1)
-all_sprites.add(C1)
 
 # Adding a new User event
 INC_SPEED = pygame.USEREVENT + 1
@@ -108,7 +117,9 @@ pygame.time.set_timer(INC_SPEED, 1000)
 # Game Loop
 while True:
 
-    # Cycles through all events occuring
+    all_sprites.add(C1)
+
+    # Cycles through all events occurring
     for event in pygame.event.get():
         if event.type == INC_SPEED:
             SPEED += 0.5
@@ -117,18 +128,24 @@ while True:
             sys.exit()
 
     DISPLAYSURF.blit(background, (0, 0))
-    scores = font_small.render(str(SCORE), True, BLACK)
+    scores = font_small.render(str(COUNTER), True, BLACK)
     DISPLAYSURF.blit(scores, (10, 10))
 
     # Moves and Re-draws all Sprites
     for entity in all_sprites:
-        entity.move()
         DISPLAYSURF.blit(entity.image, entity.rect)
+        entity.move()
+
+    #WHEN COIN COLLIDE WITH P1 IT SHOULD BE DISSAPPEAR
+    if pygame.sprite.spritecollideany(P1, coins):
+        pygame.mixer.Sound('coin.wav').play()
+        COUNTER += 1
+        all_sprites.remove(C1)
 
     # To be run if collision occurs between Player and Enemy
     if pygame.sprite.spritecollideany(P1, enemies):
         pygame.mixer.Sound('crash.wav').play()
-        time.sleep(1)
+        time.sleep(0.5)
 
         DISPLAYSURF.fill(RED)
         DISPLAYSURF.blit(game_over, (30, 250))
@@ -139,9 +156,256 @@ while True:
         time.sleep(2)
         pygame.quit()
         sys.exit()
-    P1.update()
-    for coin in coins:
-        coin.draw(DISPLAYSURF)
 
     pygame.display.update()
     FramePerSec.tick(FPS)
+
+import pygame
+import random
+import math
+
+# Making canvas
+screen = pygame.display.set_mode((900, 700))
+
+# Setting Title
+pygame.display.set_caption('GFG Paint')
+
+draw_on = False
+last_pos = (0, 0)
+
+# Radius of the Brush
+radius = 5
+
+def roundline(canvas, color, start, end, radius=1):
+    Xaxis = end[0] - start[0]
+    Yaxis = end[1] - start[1]
+    dist = max(abs(Xaxis), abs(Yaxis))
+    for i in range(dist):
+        x = int(start[0] + float(i) / dist * Xaxis)
+        y = int(start[1] + float(i) / dist * Yaxis)
+        pygame.draw.circle(canvas, color, (x, y), radius)
+
+def eraser(canvas, color, start, end):
+    rect = pygame.Rect(start, (end[0] - start[0], end[1] - start[1]))
+    pygame.draw.rect(canvas, color, rect, 2)
+
+def draw_circle(canvas, color, center, radius):
+    pygame.draw.circle(canvas, color, center, radius)
+
+def draw_rectangle(canvas, color, start, end):
+    rect = pygame.Rect(start, (end[0] - start[0], end[1] - start[1]))
+    pygame.draw.rect(canvas, color, rect, 2)
+
+def draw_triangle(canvas, color, start, end):
+    x1, y1 = start
+    x2, y2 = end
+    x3 = x1
+    y3 = y2
+    pygame.draw.polygon(canvas, color, [(x1, y1), (x2, y2), (x3, y3)])
+
+try :
+    while True :
+        e = pygame.event.wait()
+
+        if e.type == pygame.QUIT :
+            raise StopIteration
+
+        if e.type == pygame.MOUSEBUTTONDOWN :
+            # Selecting random Color Code
+            color = (random.randrange(256), random.randrange(256), random.randrange(256))
+            # Draw a single circle wheneven mouse is clicked down.
+            pygame.draw.circle(screen, color, e.pos, radius)
+            draw_on = True
+
+            # if e.type == pygame.KEYDOWN :
+            #     start_pos = e.pos
+            #     shape_type = 'circle'
+            # if e.type == pygame.KEYUP :
+            #     start_pos = e.pos
+            #     shape_type = 'eraser'
+            # if e.type == pygame.K_LEFT :
+            #     start_pos = e.pos
+            #     shape_type = 'rectangle'
+            # if e.type == pygame.K_RIGHT:
+            #     start_pos = e.pos
+            #     shape_type = 'triangle'
+
+
+             # Start drawing shapes
+            if e.button == 1:
+                start_pos = e.pos
+                shape_type = 'circle'
+            elif e.button == 3: # Right mouse button
+                start_pos = e.pos
+                shape_type = 'rectangle'
+            elif e.button == 2: # Middle mouse button
+                start_pos = e.pos
+                shape_type = 'triangle'
+
+            elif e.type == pygame.K_UP: # Middle mouse button
+                start_pos = e.pos
+                shape_type = 'eraser'
+
+        # When mouse button released it will stop drawing
+        if e.type == pygame.MOUSEBUTTONUP :
+            draw_on = False
+
+            # Finish drawing shapes
+            if shape_type == 'circle':
+                end_pos = e.pos
+                distance = math.hypot(end_pos[0] - start_pos[0], end_pos[1] - start_pos[1])
+                draw_circle(screen, color, start_pos, int(distance))
+            elif shape_type == 'rectangle':
+                end_pos = e.pos
+                draw_rectangle(screen, color, start_pos, end_pos)
+            elif shape_type == 'triangle':
+                end_pos = e.pos
+                draw_triangle(screen, color, start_pos, end_pos)
+            elif shape_type == 'eraser':
+                end_pos = e.pos
+                draw_rectangle(screen, (0, 0, 0), start_pos, end_pos)
+
+        # It will draw a continuous circle with the help of roundline function.
+        if e.type == pygame.MOUSEMOTION :
+            if draw_on :
+                pygame.draw.circle(screen, color, e.pos, radius)
+                roundline(screen, color, e.pos, last_pos, radius)
+            last_pos = e.pos
+
+        pygame.display.flip()
+
+except StopIteration :
+    pass
+
+# Quit
+pygame.quit()
+'''
+
+#SNAKE
+import pygame
+import time
+import random
+
+pygame.init()
+
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+
+dis_width = 600
+dis_height = 400
+
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Snake Game by Aidyn')
+
+clock = pygame.time.Clock()
+
+snake_block = 10
+snake_speed = 15
+
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("comicsansms", 35)
+
+
+def Your_score(score):
+    value = score_font.render("Your Score: " + str(score), True, black)
+    dis.blit(value, [0, 0])
+types_of_food = [5, 10] #5-adds 5 score;10-adds 10 score
+val = random.randint(0,1)
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+
+
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    x1 = dis_width / 2
+    y1 = dis_height / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    snake_List = []
+    Length_of_snake = 1
+
+    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+
+    while not game_over:
+
+        while game_close == True:
+            dis.fill(yellow)
+            message("You Lost! Press C-Play Again or Q-Quit", red)
+            Your_score(Length_of_snake - 1)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+            game_close = True
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(yellow)
+        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
+
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+
+        our_snake(snake_block, snake_List)
+        Your_score(Length_of_snake - 1)
+
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            Length_of_snake += 1
+
+        clock.tick(snake_speed)
+
+    pygame.quit()
+    quit()
+
+
+gameLoop()
