22d352f9dfbed40a8439e8aa87f8f693b725efa4
GizatPP <120188378+GizatPP@users.noreply.github.com>
2023-04-04 17:48:52 +0600

    Update lab_9.py

--- lab_9.py
+++ lab_9.py
@@ -1,5 +1,4 @@
 
-'''
 #racer
 # Imports
 import pygame, sys
@@ -159,7 +158,8 @@ while True:
 
     pygame.display.update()
     FramePerSec.tick(FPS)
-
+    
+#paint
 import pygame
 import random
 import math
@@ -279,7 +279,6 @@ except StopIteration :
 
 # Quit
 pygame.quit()
-'''
 
 #SNAKE
 import pygame
@@ -288,18 +287,14 @@ import random
 
 pygame.init()
 
-white = (255, 255, 255)
 yellow = (255, 255, 102)
-black = (0, 0, 0)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
+
 
 dis_width = 600
 dis_height = 400
 
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game by Aidyn')
+pygame.display.set_caption('SNAKE')
 
 clock = pygame.time.Clock()
 
@@ -311,7 +306,7 @@ score_font = pygame.font.SysFont("comicsansms", 35)
 
 
 def Your_score(score):
-    value = score_font.render("Your Score: " + str(score), True, black)
+    value = score_font.render("Your Score: " + str(score), True, BLACK)
     dis.blit(value, [0, 0])
 types_of_food = [5, 10] #5-adds 5 score;10-adds 10 score
 val = random.randint(0,1)
@@ -346,18 +341,10 @@ def gameLoop():
 
         while game_close == True:
             dis.fill(yellow)
-            message("You Lost! Press C-Play Again or Q-Quit", red)
+            message("You Lost!", BLACK)
             Your_score(Length_of_snake - 1)
             pygame.display.update()
-
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        game_over = True
-                        game_close = False
-                    if event.key == pygame.K_c:
-                        gameLoop()
-
+            
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
